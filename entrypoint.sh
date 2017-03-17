a9c9762a468fb7c505dfdcc83cf6a747796d6759
elsesky <elsesky@gmail.com>
2017-03-17 17:01:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ service crond start
 echo "start ssr"
 /usr/bin/python /elsesky/shadowsocksr/shadowsocks/server.py -c /elsesky/shadowsocksr/user-config.json -d start
 echo "start sshd"
-/usr/sbin/sshd -D
+/usr/sbin/sshd -d

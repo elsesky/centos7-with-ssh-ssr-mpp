b162e5756d4747ee2721b29d7ad8b00b5bc99568
elsesky <elsesky@gmail.com>
2017-03-17 16:37:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 ping www.baidu.com -c 5
 
-
+echo "start cron"
+service crond start
+echo "start sshd"
+/usr/sbin/sshd -D
+echo "start ssr"
 /usr/bin/python /elsesky/shadowsocksr/shadowsocks/server.py -c /elsesky/shadowsocksr/user-config.json -d start

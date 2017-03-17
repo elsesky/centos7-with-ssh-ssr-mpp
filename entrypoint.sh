a471181329286b8042d7ba3aa4ef56b216562735
elsesky <elsesky@gmail.com>
2017-03-17 14:53:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 ping www.baidu.com -c 5
 
 
-/usr/bin/python /ssr/shadowsocks/server.py /elsesky/shadowsocksr/shadowsocks/server.py -c /elsesky/shadowsocksr/user-config.json -d start
+/usr/bin/python /elsesky/shadowsocksr/shadowsocks/server.py -c /elsesky/shadowsocksr/user-config.json -d start

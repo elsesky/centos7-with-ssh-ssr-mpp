9127ffa2769e67609d764ed46a67a5d8ad8a664d
elsesky <elsesky@gmail.com>
2017-03-20 14:08:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #ping www.baidu.com -c 5
 
-ARGS=`getopt -o r:p:o:a:m:P:A: --long p1:,p2:,p3:,p4:,p5:,p6:,p7:,p8:,p9:,p10:,p11:,p12:,p13:,p14:,p15:,p16:,p17:,p18:,p19: -n '1.sh' -- "$@"`
+ARGS=`getopt -o r:p:o:a:m:P:A: --long p1:,p2:,p3:,p4:,p5:,p6:,p7:,p8:,p9:,p10:,p11:,p12:,p13:,p14:,p15:,p16:,p17:,p18:,p19: -n 'entrypoint.sh' -- "$@"`
 if [ $? != 0 ]; then
     echo "Args error..."
     exit 1
